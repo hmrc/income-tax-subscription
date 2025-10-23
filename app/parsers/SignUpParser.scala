ddbf606cf4ff9c9189bb992f6ea835e6ac51939a
ymabdulahi <68248111+ymabdulahi@users.noreply.github.com>
2025-10-23 11:56:52 +0100

    removed old signupresponse http reads in parser

--- app/parsers/SignUpParser.scala
+++ app/parsers/SignUpParser.scala
@@ -27,25 +27,6 @@ object SignUpParser {
 
   type PostSignUpResponse = Either[ErrorModel, SignUpResponse]
 
-  implicit val signUpResponseHttpReads: HttpReads[PostSignUpResponse] = {
-    (_: String, _: String, response: HttpResponse) =>
-      response.status match {
-        case OK => response.json.validate[SignUpSuccess] match {
-          case JsSuccess(value, _) => Right(value)
-          case JsError(_) => Left(ErrorModel(OK, s"Failed to read Json for MTD Sign Up Response"))
-        }
-        case UNPROCESSABLE_ENTITY => (response.json \ "failures" \\ "code").map(_.validate[String]).headOption match {
-          case Some(JsSuccess(CustomerAlreadySignedUp, _)) => Right(SignUpResponse.AlreadySignedUp)
-          case Some(JsSuccess(code, _)) => Left(ErrorModel(UNPROCESSABLE_ENTITY, code))
-          case _ => Left(ErrorModel(UNPROCESSABLE_ENTITY, s"Failed to read Json for MTD Sign Up Response"))
-        }
-        case FORBIDDEN => throw SignUpParserException(s"Failed to read Json for MTD Sign Up Response", FORBIDDEN)
-        case status => Left(ErrorModel(status, response.body))
-      }
-  }
-
-  private val CustomerAlreadySignedUp: String = "CUSTOMER_ALREADY_SIGNED_UP"
-
   implicit val hipSignUpResponseHttpReads: HttpReads[PostSignUpResponse] = {
     (_: String, _: String, response: HttpResponse) =>
       response.status match {
