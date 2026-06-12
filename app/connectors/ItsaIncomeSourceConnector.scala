94a39113545836ef201b6b8a08ee225aa4111d27
NoorMhd7 <146771080+NoorMhd7@users.noreply.github.com>
2026-06-12 11:37:35 +0100

    ITSASU-5207

--- app/connectors/ItsaIncomeSourceConnector.scala
+++ app/connectors/ItsaIncomeSourceConnector.scala
@@ -26,6 +26,7 @@ import parsers.ITSAIncomeSourceParser.*
 import play.api.libs.json.Json
 import play.api.mvc.Request
 import services.monitoring.AuditService
+import services.monitoring.CreateIncomeSourcesAudit
 import uk.gov.hmrc.http.client.HttpClientV2
 import uk.gov.hmrc.http.{HeaderCarrier, Retries}
 
@@ -49,6 +50,14 @@ class ItsaIncomeSourceConnector @Inject()(val httpClient: HttpClientV2,
                          (implicit hc: HeaderCarrier, request: Request[_]): Future[PostITSAIncomeSourceResponse] = {
     auditService.extendedAudit(SignUpAudit(agentReferenceNumber, createIncomeSources, appConfig.getHipAuthToken)) flatMap {
       _ => updateETMP(mtdbsaRef, createIncomeSources)
+    } map { result =>
+      auditService.extendedAudit(CreateIncomeSourcesAudit(
+        agentReferenceNumber   = agentReferenceNumber,
+        nino                   = createIncomeSources.nino,
+        mtdItsaReferenceNumber = mtdbsaRef,
+        result                 = result.map(_ => ())
+      ))
+      result
     }
   }
 
