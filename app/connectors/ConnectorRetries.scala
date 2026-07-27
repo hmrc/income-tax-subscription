a6392b1e4d14b66db973608087965db6bb7570d8
Patrick Lucas <paddynski-opencast@users.noreply.github.com>
2026-07-27 14:18:04 +0100

    [ITSASU-5326]

--- app/connectors/ConnectorRetries.scala
+++ app/connectors/ConnectorRetries.scala
@@ -63,8 +63,7 @@ trait ConnectorRetries extends Logging {
     val result = loop(intervals(apiNumber))
     result.onComplete {
       case Success(Left(error)) => logError(error, level)
-      case Success(Right(_)) => {}
-      case Failure(exception) => throw exception
+      case _ => {}
     }
     result
   }
